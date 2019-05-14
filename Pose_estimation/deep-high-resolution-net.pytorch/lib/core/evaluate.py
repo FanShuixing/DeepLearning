2afae2bb9bab3a70d3e42b1f40fb92fa9c7a7eeb
pan <1094170587@qq.com>
2019-05-14 12:04:17 +0800

    update

--- Pose_estimation/deep-high-resolution-net.pytorch/lib/core/evaluate.py
+++ Pose_estimation/deep-high-resolution-net.pytorch/lib/core/evaluate.py
@@ -14,11 +14,14 @@ from core.inference import get_max_preds
 
 
 def calc_dists(preds, target, normalize):
+    #preds:(48,16,2)
+    #target:(48,16,2)
+    #normalize:(48,2)
     preds = preds.astype(np.float32)
     target = target.astype(np.float32)
-    dists = np.zeros((preds.shape[1], preds.shape[0]))
-    for n in range(preds.shape[0]):
-        for c in range(preds.shape[1]):
+    dists = np.zeros((preds.shape[1], preds.shape[0]))  #(16,48)
+    for n in range(preds.shape[0]): #48 
+        for c in range(preds.shape[1]): #16
             if target[n, c, 0] > 1 and target[n, c, 1] > 1:
                 normed_preds = preds[n, c, :] / normalize[n]
                 normed_targets = target[n, c, :] / normalize[n]
@@ -44,18 +47,31 @@ def accuracy(output, target, hm_type='gaussian', thr=0.5):
     but uses ground truth heatmap rather than x,y locations
     First value to be returned is average accuracy across 'idxs',
     followed by individual accuracies
+    
+    input:
+        output:(48,16,64,64)
+        target:(48,16,64,64)
+    return:
+        acc:一维向量，长度为17，第0个位置上的值为avg_acc，其余位置值的计算方式：
+            step1:先通过cals_dists计算pred,target之间的距离，返回dists:(16,48)的array
+            step2:对dists中的数据进行行遍历,如dists[0]表示一个batch_size对应的keypoint0的预测值和真实值的距离，将dists[0]传入dist_acc中，
+                    计算的是 距离度量小于0.5的个数/总个数(batch_size或者小于batch_size的数)
+            
+        avg_acc:acc长度为17，除第0个位置外，其它位置的数据表示对应于一个keypoint，batch_size个数据的正确率，avg_acc是对acc中的数求平均所得
+        cnt:对acc大于0的数计数
+        pred:
     '''
-    idx = list(range(output.shape[1]))
+    idx = list(range(output.shape[1])) #一维向量，len(idx)=16
     norm = 1.0
     if hm_type == 'gaussian':
-        pred, _ = get_max_preds(output)
-        target, _ = get_max_preds(target)
+        pred, _ = get_max_preds(output)  #(48,16,2)
+        target, _ = get_max_preds(target)  #(48,16,2)
         h = output.shape[2]
         w = output.shape[3]
-        norm = np.ones((pred.shape[0], 2)) * np.array([h, w]) / 10
-    dists = calc_dists(pred, target, norm)
+        norm = np.ones((pred.shape[0], 2)) * np.array([h, w]) / 10  #(48,2)
+    dists = calc_dists(pred, target, norm)  #(16,48)
 
-    acc = np.zeros((len(idx) + 1))
+    acc = np.zeros((len(idx) + 1))  #一维0向量，len(acc)=17
     avg_acc = 0
     cnt = 0
 
--- Pose_estimation/deep-high-resolution-net.pytorch/lib/dataset/mpii.py
+++ Pose_estimation/deep-high-resolution-net.pytorch/lib/dataset/mpii.py
@@ -32,7 +32,7 @@ class MPIIDataset(JointsDataset):
 
         self.upper_body_ids = (7, 8, 9, 10, 11, 12, 13, 14, 15)
         self.lower_body_ids = (0, 1, 2, 3, 4, 5, 6)
-        self.root='/input0/MPII'
+
         self.db = self._get_db()
 
         if is_train and cfg.DATASET.SELECT_DATA:
@@ -42,19 +42,6 @@ class MPIIDataset(JointsDataset):
 
     def _get_db(self):
         # create train/val split
-        '''
-        
-        returns:gt_db,type:list,value_type:dict
-        通过加载json文件得到以下信息
-        image:图片的路径
-        center:加载json中的center,为了cropping limbs ，会对center和scale稍作调整，c[0]不变，c[1]=c[1]+15*scale[1]，然后c=c-1
-        scale:scale原本为np.array([scale,scale]),同上理由，改为x1.5倍
-        joints_3d:size=(16,3),前2列的值为json中加载的joints-1,最后一列依然为0
-        joints_3d_vis:size=(16,3)，前2列的值为json中加载的'joints_vis',最后一列为0
-        filename:''
-        imgnum:0
-        
-        '''
         file_name = os.path.join(
             self.root, 'annot', self.image_set+'.json'
         )
@@ -80,7 +67,6 @@ class MPIIDataset(JointsDataset):
             joints_3d = np.zeros((self.num_joints, 3), dtype=np.float)
             joints_3d_vis = np.zeros((self.num_joints,  3), dtype=np.float)
             if self.image_set != 'test':
-                #a['joints']为16行2列，里面对应的是16个关节的坐标，a['joints_vis']为一维的list,里面有16个值，值只有0和1，表示16个关节点是否可见
                 joints = np.array(a['joints'])
                 joints[:, 0:2] = joints[:, 0:2] - 1
                 joints_vis = np.array(a['joints_vis'])
@@ -104,36 +90,61 @@ class MPIIDataset(JointsDataset):
                     'imgnum': 0,
                 }
             )
-
         return gt_db
 
     def evaluate(self, cfg, preds, output_dir, *args, **kwargs):
         # convert 0-based index to 1-based index
+        '''
+        此方法是被/lib/core/function.py中的validate()函数调用，用于评估验证集的pck值。
+        
+        step1:从数据中加载matlab文件,/input0/MPII/annot/gt_valid.mat,从mat中读取出需要的值
+        step2:计算预测值和真实值之间的距离，pos_pred_src为预测的坐标值，经过变换后size为(16,2,2958),pos_gt_src为真实值，size为(16,2,2958)
+              计算两者的差，然后按行计算第二范数，得到的uv_error的size为(16,2958)
+        step3:headboxes_src的size为(2,2,2958),2*2表示图片中头部的框，坐标为(x1,y1),(x2,y2)的形式,先计算(x1,y1)和(x2,y2)之间的距离，得到(2,2958)
+              大小的矩阵，再对列做第二范数，得到(2958,)大小的矩阵。表示2958张图片，每一张图片头部框坐标(x1,y1),(x2,y2)的第二范数。headsizes*=SC_BIAS。
+              计算scale,scale为将headsizes复制16行的数组。大小为(16,2958).每一行表示的都是2958张图片中头部框对应坐标的第二范数值。
+        step4:计算scale_uv_error.之间计算出uv_err,size为(16,2958),含义是2958张图片，每一张图片对应的16个关节点的真实值和预测值之间的距离，scale_uv_error
+              将uv_err除以scale.
+        step5:将scale_uv_error中小于threshold的值按行计数，再除以jnt_count。得到PCKh为(16,)，表示2958张图片中，每一个关节点距离小于threhold的数除以
+              这个关节在2958张图片中出现的总数。
+        
+        input:
+            cfg:配置文件
+            preds:(imgnums,16,2),preds是模型预测的关节点的坐标，坐标是相对于原图
+            output_dir:
+            args:接收
+            kwargs:接收
+            
+        output:
+            name_value:返回的PCKh中对应的值
+            name_value['Mean']:name_value中['Mean']对应的是16个关节点的pck值取平均，['Mean@0.1']是将阈值为0.1,
+                               即将距离的threshold从0.5改调为0.1的16个关节点的pck的平均值
+            
+        '''
         preds = preds[:, :, 0:2] + 1.0
-
         if output_dir:
             pred_file = os.path.join(output_dir, 'pred.mat')
-            savemat(pred_file, mdict={'preds': preds})
+            savemat(pred_file, mdict={'preds': preds}
 
         if 'test' in cfg.DATASET.TEST_SET:
             return {'Null': 0.0}, 0.0
 
         SC_BIAS = 0.6
         threshold = 0.5
-
-        gt_file = os.path.join(cfg.DATASET.ROOT,
+        #step1
+        gt_file = os.path.join('/input0/MPII/',
                                'annot',
                                'gt_{}.mat'.format(cfg.DATASET.TEST_SET))
-        gt_file='/input0/MPII/annot/gt_valid.mat'
-
         gt_dict = loadmat(gt_file)
         dataset_joints = gt_dict['dataset_joints']
-        jnt_missing = gt_dict['jnt_missing']
-        pos_gt_src = gt_dict['pos_gt_src']
-        headboxes_src = gt_dict['headboxes_src']
-
+        jnt_missing = gt_dict['jnt_missing']  #(16,2958)
+        #pos_gt_src的值为valid中图像的joints，即关节点的坐标，这个坐标不是相对于255*255或64*64的坐标，而是相对于原图大小的坐标  
+        pos_gt_src = gt_dict['pos_gt_src'] #（16，2，2958）
+        headboxes_src = gt_dict['headboxes_src']  #(2,2,2958),为头部框的坐标，形式为(x1,y1),(x2,y2)
+        #可以通过pos_gt_src中的值得知pos_pred_src中的值为预测出来的关节点的坐标，同样是相对于原图的坐标
         pos_pred_src = np.transpose(preds, [1, 2, 0])
-
+                    
+        #如下返回的是索引
         head = np.where(dataset_joints == 'head')[1][0]
         lsho = np.where(dataset_joints == 'lsho')[1][0]
         lelb = np.where(dataset_joints == 'lelb')[1][0]
@@ -149,23 +160,25 @@ class MPIIDataset(JointsDataset):
         rank = np.where(dataset_joints == 'rank')[1][0]
         rhip = np.where(dataset_joints == 'rhip')[1][0]
 
+        #step2
         jnt_visible = 1 - jnt_missing
-        uv_error = pos_pred_src - pos_gt_src
-        uv_err = np.linalg.norm(uv_error, axis=1)
-        headsizes = headboxes_src[1, :, :] - headboxes_src[0, :, :]
-        headsizes = np.linalg.norm(headsizes, axis=0)
+        uv_error = pos_pred_src - pos_gt_src 
+        uv_err = np.linalg.norm(uv_error, axis=1) #(16,2958)
+        headsizes = headboxes_src[1, :, :] - headboxes_src[0, :, :] #(2,2958),(x2,y2)-(x1,y1)
+        headsizes = np.linalg.norm(headsizes, axis=0) #(2958,),求二范数，结合上面这步，想当于求(x1,y1),(x2,y2)之间的距离
         headsizes *= SC_BIAS
-        scale = np.multiply(headsizes, np.ones((len(uv_err), 1)))
-        scaled_uv_err = np.divide(uv_err, scale)
+        #headsizes为(2958,),len(uv_err)=16，scale为(16,2958),相当于将headsizes复制了16行
+        scale = np.multiply(headsizes, np.ones((len(uv_err), 1)))  #(16,2958)
+        scaled_uv_err = np.divide(uv_err, scale)  #(16,2958)
         scaled_uv_err = np.multiply(scaled_uv_err, jnt_visible)
-        jnt_count = np.sum(jnt_visible, axis=1)
+        jnt_count = np.sum(jnt_visible, axis=1)  #(16,)
         less_than_threshold = np.multiply((scaled_uv_err <= threshold),
-                                          jnt_visible)
-        PCKh = np.divide(100.*np.sum(less_than_threshold, axis=1), jnt_count)
+                                          jnt_visible)  #(16,2958)
+        PCKh = np.divide(100.*np.sum(less_than_threshold, axis=1), jnt_count) #(16,)
 
         # save
-        rng = np.arange(0, 0.5+0.01, 0.01)
-        pckAll = np.zeros((len(rng), 16))
+        rng = np.arange(0, 0.5+0.01, 0.01)  #len(rng)=51
+        pckAll = np.zeros((len(rng), 16))  #(51,16)
 
         for r in range(len(rng)):
             threshold = rng[r]
